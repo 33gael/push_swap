6c4d95beafecc17274f27f5040cda487d0ea0e18
Gaël <33gael.dev@gmail.com>
2026-01-16 14:44:45 +0100

    stack initv2 commit

--- src/stack_init.c
+++ src/stack_init.c
@@ -6,7 +6,7 @@
 /*   By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 10:43:29 by gaeducas          #+#    #+#             */
-/*   Updated: 2026/01/16 14:36:27 by gaeducas         ###   ########.fr       */
+/*   Updated: 2026/01/16 14:44:34 by gaeducas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ t_node	*create_node(int value)
 
 void	stack_add_back(t_node **stack, t_node *new_node)
 {
-	t_node *current;
+	t_node	*current;
 
 	if (!new_node)
 		return ;
@@ -39,4 +39,41 @@ void	stack_add_back(t_node **stack, t_node *new_node)
 	while (current->next)
 		current = current->next;
 	current->next = new_node;
+}
+
+void	free_stack(t_node **stack)
+{
+	t_node	*tmp;
+
+	if (!stack || !*stack)
+		return ;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+	*stack = NULL;
+}
+
+t_node	*init_stack(int *numbers, int count)
+{
+	t_node	*stack;
+	t_node	*new_node;
+	int		i;
+
+	stack = NULL;
+	i = 0;
+	while (i < count)
+	{
+		new_node = create_node(numbers[i]);
+		if (!new_node)
+		{
+			free_stack(&stack);
+			return (NULL);
+		}
+		stack_add_back(&stack, new_node);
+		i++;
+	}
+	return (stack);
 }
